a1bc0fcb289989b8bfd2e0adb0f776099b6f7606
ChefEric <173023571+chef-eric@users.noreply.github.com>
2025-07-31 22:47:45 +0800

    fix solana quote refresh by hashing nonce

--- apps/web/src/quoter/atom/bestSVMOrderAtom.ts
+++ apps/web/src/quoter/atom/bestSVMOrderAtom.ts
@@ -2,81 +2,75 @@ import { getBestSolanaTrade } from '@pancakeswap/solana-router-sdk'
 import { SPLToken, TradeType, UnifiedCurrencyAmount } from '@pancakeswap/swap-sdk-core'
 import { Loadable } from '@pancakeswap/utils/Loadable'
 import { withTimeout } from '@pancakeswap/utils/withTimeout'
-import { atomFamily } from 'jotai/utils'
 import { solanaUserSlippageAtomWithLocalStorage } from '@pancakeswap/utils/user'
 import { QUOTE_TIMEOUT } from 'quoter/consts'
 import { parseSVMTradeIntoSVMOrder } from 'quoter/utils/svm-utils/parseSVMTradeIntoSVMOrder'
+import { atomFamily } from 'jotai/utils'
 import { type InterfaceOrder } from 'views/Swap/utils'
 import type { SVMQuoteQuery } from '../quoter.types'
+import { isEqualQuoteQuery } from '../utils/PoolHashHelper'
 import { atomWithLoadable } from './atomWithLoadable'
 
-export const bestSVMOrderAtom = atomFamily(
-  (_option: SVMQuoteQuery) => {
-    return atomWithLoadable<InterfaceOrder>(async (get) => {
-      const { baseCurrency, currency, amount, tradeType, address } = _option
-      const userSlippageTolerance = get(solanaUserSlippageAtomWithLocalStorage)
+export const bestSVMOrderAtom = atomFamily((_option: SVMQuoteQuery) => {
+  return atomWithLoadable<InterfaceOrder>(async (get) => {
+    const { baseCurrency, currency, amount, tradeType, address } = _option
+    const userSlippageTolerance = get(solanaUserSlippageAtomWithLocalStorage)
 
-      // Early validation
-      if (!baseCurrency || !currency || !amount || tradeType === undefined) {
+    // Early validation
+    if (!baseCurrency || !currency || !amount || tradeType === undefined) {
+      return Loadable.Nothing<InterfaceOrder>()
+    }
+
+    const controller = new AbortController()
+    // const perf = get(quoteTraceAtom(_option))
+    // perf.tracker.track('start')
+
+    try {
+      const query = withTimeout(
+        async () => {
+          const [inputCurrency, outputCurrency] =
+            tradeType === TradeType.EXACT_INPUT ? [baseCurrency, currency] : [currency, baseCurrency]
+          // Parse response to SVM order format
+          const solTradeRoute = await getBestSolanaTrade({
+            // TODO: need to remove as SPLToken
+            inputCurrency: inputCurrency as SPLToken,
+            outputCurrency: outputCurrency as SPLToken,
+            amount: amount as UnifiedCurrencyAmount<SPLToken>,
+            tradeType: tradeType as TradeType,
+            slippageBps: userSlippageTolerance,
+            account: address,
+          })
+
+          //   perf.tracker.success(svmOrder)
+          return solTradeRoute
+        },
+        {
+          ms: QUOTE_TIMEOUT,
+          abort: () => {
+            controller.abort()
+          },
+        },
+      )
+
+      let bestOrder: InterfaceOrder | undefined
+
+      const trade = await query()
+
+      // if result.type is SVMOrder, can safely cast to InterfaceOrder
+      if (trade) {
+        bestOrder = parseSVMTradeIntoSVMOrder(trade, _option)
+      }
+
+      if (!bestOrder) {
         return Loadable.Nothing<InterfaceOrder>()
       }
 
-      const controller = new AbortController()
-      // const perf = get(quoteTraceAtom(_option))
-      // perf.tracker.track('start')
-
-      try {
-        const query = withTimeout(
-          async () => {
-            const [inputCurrency, outputCurrency] =
-              tradeType === TradeType.EXACT_INPUT ? [baseCurrency, currency] : [currency, baseCurrency]
-            // Parse response to SVM order format
-            const solTradeRoute = await getBestSolanaTrade({
-              // TODO: need to remove as SPLToken
-              inputCurrency: inputCurrency as SPLToken,
-              outputCurrency: outputCurrency as SPLToken,
-              amount: amount as UnifiedCurrencyAmount<SPLToken>,
-              tradeType: tradeType as TradeType,
-              slippageBps: userSlippageTolerance,
-              account: address,
-            })
-
-            //   perf.tracker.success(svmOrder)
-            return solTradeRoute
-          },
-          {
-            ms: QUOTE_TIMEOUT,
-            abort: () => {
-              controller.abort()
-            },
-          },
-        )
-
-        let bestOrder: InterfaceOrder | undefined
-
-        const trade = await query()
-
-        // if result.type is SVMOrder, can safely cast to InterfaceOrder
-        if (trade) {
-          bestOrder = parseSVMTradeIntoSVMOrder(trade, _option)
-        }
-
-        if (!bestOrder) {
-          return Loadable.Nothing<InterfaceOrder>()
-        }
-
-        return Loadable.Just<InterfaceOrder>(bestOrder)
-      } catch (error) {
-        return Loadable.Fail<InterfaceOrder>(error)
-        //   perf.tracker.fail(error)
-      } finally {
-        //   perf.tracker.report()
-      }
-    })
-  },
-  (a, b) =>
-    a.baseCurrency?.wrapped?.address === b.baseCurrency?.wrapped?.address &&
-    a.currency?.wrapped?.address === b.currency?.wrapped?.address &&
-    a.amount?.toExact() === b.amount?.toExact() &&
-    a.tradeType === b.tradeType,
-)
+      return Loadable.Just<InterfaceOrder>(bestOrder)
+    } catch (error) {
+      return Loadable.Fail<InterfaceOrder>(error)
+      //   perf.tracker.fail(error)
+    } finally {
+      //   perf.tracker.report()
+    }
+  })
+}, isEqualQuoteQuery)
